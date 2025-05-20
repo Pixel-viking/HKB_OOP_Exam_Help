760de461ff1e7fdd32f014ab295f01dc991aee8e
Pixel-viking <carleirikdj@gmail.com>
2025-05-20 09:13:07 +0200

    Changed player

--- src/Player.java
+++ src/Player.java
@@ -1,20 +1,7 @@
-import java.util.ArrayList;
 
-public class Player {
-    public String name;
-    private final ArrayList<Unit> units;
-
-    public Player(String name, ArrayList<Unit> units) {
-        this.name = name;
-        this.units = units;
-    }
-
-    public boolean hasUnits() {
-        if (units.isEmpty()) return true;
-        return true;
-    }
-
-    public String getName() {
-        return name;
+public class Player extends Entity
+{
+    Player(int health, int damageMin, int damageMax) {
+        super(health, damageMin, damageMax);
     }
 }
